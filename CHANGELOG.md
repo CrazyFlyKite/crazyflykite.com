49cb0f008a1a6ecf259c600f9404d36c4e7a493d
CrazyFlyKite <karpenkoartem2846@gmail.com>
2026-02-12 18:29:32 +0100

    1.0.1 - Adapted everything for mobile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## 1.0.1 | 12.02.2026
+
+- Adapted everything for mobile
+- Added more projects
+- Removed problematic smooth scroll transition
+- Improved rate requirements
+
 ## 1.0.0 | 11.02.2026
 
 - Initial release
--- public/index.js
+++ public/index.js
@@ -1,14 +1,3 @@
-// Scroll
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) entry.target.classList.add('show');
-		else entry.target.classList.remove('show');
-	})
-});
-
-const hiddenElements = document.querySelectorAll('section');
-hiddenElements.forEach((element) => observer.observe(element));
-
 // Age
 function calculateAge(birthDate) {
 	const today = new Date();
